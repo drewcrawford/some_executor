e42388740457aa0e5331fda2ce6cc052b0e407ab
Drew Crawford <drew@sealedabstract.com>
2025-11-25 03:06:29 -0600

    re-verify baseline

--- src/static_last_resort.rs
+++ src/static_last_resort.rs
@@ -424,6 +424,7 @@ mod tests {
     }
 
     /// Test 4: Does executor work inside a spawned thread?
+    #[cfg(not(target_arch = "wasm32"))]
     #[test_executors::async_test]
     async fn isolation_test_executor_in_thread() {
         #[cfg(target_arch = "wasm32")]
